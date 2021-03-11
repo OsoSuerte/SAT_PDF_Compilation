271197c042be8c9b523d1928ef2992d1220e12bb
OsoSuerte <69661970+OsoSuerte@users.noreply.github.com>
2021-03-11 08:32:19 -0700

    Add files via upload

--- full_sat.py
+++ full_sat.py
@@ -1,92 +1,71 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import PyPDF2
-import pdfminer
 import tabula
 import csv
 import sqlite3
 import glob, os
-from pdfminer import high_level
-import os
 from urllib.parse import urljoin
 
+url = "https://reports.collegeboard.org/sat-suite-program-results"  # url for website with target PDFS
 
-url = "https://reports.collegeboard.org/sat-suite-program-results"
-
-#If there is no such folder, the script will create one automatically
+# Location to store PDFs. If there is no such folder, the script will create one
 folder_location = r'C:\Coding\Pycharm projects\SATS\pdfs'
 if not os.path.exists(folder_location):os.mkdir(folder_location)
 
+# Locate, download and rename the pdf files using a unique portion of each link.
 response = requests.get(url)
 soup= BeautifulSoup(response.text, "html.parser")
 for link in soup.select("a[href$='.pdf']"):
-    #Name the pdf files using the last portion of each link which are unique in this case
     filename = os.path.join(folder_location,link['href'].split('-')[-6] +'.pdf')
     with open(filename, 'wb') as f:
         f.write(requests.get(urljoin(url,link['href'])).content)
 
-
+# Iterates through all downloaded PDFs and converts a selected portion of each to an indivivual .csv file
 all_pdf = []
 os.chdir("C:\Coding\Pycharm projects\SATS\pdfs")
 for file in glob.glob("*.pdf"):
     all_pdf.append(file)
-
-# convert PDF into CSV file
 for pdf in all_pdf:
-
     tabula.convert_into(os.path.join(r"C:\Coding\Pycharm projects\SATS\pdfs", pdf), os.path.join(r"C:\Coding\Pycharm projects\SATS\csvs", pdf.split('.')[0] + '.csv'), output_format="csv", pages='4')
 
-
+# lines 32-62 iterates through csv files to clean data and create a dictionary with all data.
 all_csv = []
 os.chdir("C:\Coding\Pycharm projects\SATS\csvs")
 for file in glob.glob("*.csv"):
     all_csv.append(file)
 
-all_states = {
-
-}
-
+all_states = {}  # Dictionary containing all cleaned data
 for each in all_csv:
-
-    if each == 'group.csv':
-        continue
-    if each == 'iowa.csv':
+    if each == 'group.csv':  # 'group' is not used for this part of project.
         continue
     state = str(each.split('.')[0])
-
     stats = []
     with open(os.path.join(r"C:\Coding\Pycharm projects\SATS\csvs", each)) as data_csv:
         data = csv.reader(data_csv)
-
-        for x in data:
-            if x[0] == '':
+        for i in data:
+            if i[0] == '':
              continue
-            if x[1] == 'Test Takers':
+            if i[1] == 'Test Takers':
                 continue
-            stats.append(x)
+            stats.append(i)
 
-    statdic = {
-    }
+    statdic = {}
     current = {}
-    for i in stats:
+    for i in stats: # used to select desired data individual csv files combined for dictionary
         try:
             statdic[i[0]] = {'group': i[0], 'total': i[1].split()[0], 'percent_of_test_takers': i[1].split()[-1], 'total_mean_score': i[2], 'erw_mean_score': i[3], 'math_mean_score': i[4], 'met_both_benchmarks': i[5], 'met_erw_bench_mark': i[6].split(' ')[0], 'met_math_bench_mark': i[6].split(' ')[-1], 'met_niether_bench_mark': i[7]}
-
-        except: continue
-
+        except: continue # could be worked out but is currently used if a PDF has format discrepancies
         current.update(statdic)
-
         try:
             all_states.update({state: current})
         except: pass
 
-with open(r'C:\Coding\Pycharm projects\SATS\fin.csv', 'w') as csv_file:
+# Used to export data to two files for further use. One is formated as a dictionary other as DataFrame
+with open(r'C:\Coding\Pycharm projects\SATS\dict_output.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     for key, value in all_states.items():
        writer.writerow([key, value])
 all_states_df = pd.DataFrame(all_states)
-all_states_df.to_csv(r'C:\Coding\Pycharm projects\SATS\findf.csv', index = False)
+all_states_df.to_csv(r'C:\Coding\Pycharm projects\SATS\dataframe_output.csv', index = False)
 
